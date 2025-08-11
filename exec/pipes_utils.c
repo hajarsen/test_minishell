0de33af07e5fc2c8d0c36ce2cc6ae2fe74f36793
hajarsen <hajarsennane1@gmail.com>
2025-08-11 23:41:40 +0100

    Update pipes_utils.c

--- exec/pipes_utils.c
+++ exec/pipes_utils.c
@@ -2,22 +2,6 @@
 
 void save_exit_status(t_glb *glb, int status_code)
 {
-    int fd;
-    ssize_t bytes_written;
-
-    fd = open(SHELL_CHILD_PID_FILE, O_WRONLY | O_CREAT | O_APPEND, 0777);
-    if (fd < 0)
-    {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
-    bytes_written = write(fd, &status_code, sizeof(status_code));
-    if (bytes_written < 0)
-    {
-        perror("write");
-        exit(EXIT_FAILURE);
-    }
-    close(fd);
     glb->exit_status = status_code;
 }
 
