8d7ccb77e059e51a8cbdecfc8c7c2768e71d6d9c
hajar.sen <hajarsennane1@gmail.com>
2025-08-12 06:34:14 +0100

    done

--- exec/pipes_utils.c
+++ exec/pipes_utils.c
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipes_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsennane <hsennane@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 05:16:47 by hsennane          #+#    #+#             */
+/*   Updated: 2025/08/12 05:16:48 by hsennane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void save_exit_status(t_glb *glb, int status_code)
+void	save_exit_status(t_glb *glb, int status_code)
 {
-    glb->exit_status = status_code;
+	glb->exit_status = status_code;
 }
 
-int has_pipe(t_tokenizer *tokens)
+int	has_pipe(t_tokenizer *tokens)
 {
-    while (tokens)
-    {
-        if (tokens->op == PIPE)
-            return 1;
-        tokens = tokens->next;
-    }
-    return 0;
+	while (tokens)
+	{
+		if (tokens->op == PIPE)
+			return (1);
+		tokens = tokens->next;
+	}
+	return (0);
 }
