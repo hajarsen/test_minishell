9da805ad9a436ddfa87bbcb9f46d619313b9338a
hajarsen <hajarsennane1@gmail.com>
2025-08-12 03:05:12 +0100

    Update signals_setup.c

--- exec/signals_setup.c
+++ exec/signals_setup.c
@@ -1,32 +1,29 @@
 #include "minishell.h"
 
-static void setup_signal_action(int signum, void (*handler)(int), int flags)
+static void	setup_signal_action(int signum, void (*handler)(int), int flags)
 {
-    struct sigaction act;
+	struct sigaction	act;
 
-    sigemptyset(&act.sa_mask);
-    act.sa_handler = handler;
-    act.sa_flags = flags;
-    sigaction(signum, &act, NULL);
+	sigemptyset(&act.sa_mask);
+	act.sa_handler = handler;
+	act.sa_flags = flags;
+	sigaction(signum, &act, NULL);
 }
 
-void setup_signals(void)
+void	setup_signals(void)
 {
-    setup_signal_action(SIGINT, signal_handler_general, SA_RESTART);
-    setup_signal_action(SIGQUIT, SIG_IGN, 0);
+	setup_signal_action(SIGINT, signal_handler_general, SA_RESTART);
+	setup_signal_action(SIGQUIT, SIG_IGN, 0);
 }
 
-void set_signal_handler(t_tokenizer *token)
+void	set_signal_handler(t_tokenizer *token)
 {
-    if (token && token->op == LESS_LESS)
+	/* Only specialize for heredoc; otherwise keep whatever is configured */
+	if (token && token->op == LESS_LESS)
 	{
-        setup_signal_action(SIGINT, signal_handler_heredoc, SA_RESTART);
-        setup_signal_action(SIGQUIT, signal_handler_heredoc, SA_RESTART);
-    }
-    else
-    {
-        setup_signal_action(SIGINT, signal_handler_input, SA_RESTART);
-        setup_signal_action(SIGQUIT, signal_handler_input, SA_RESTART);
-    }
+		/* Parent is not in readline; use minimal handler */
+		setup_signal_action(SIGINT, signal_handler_heredoc, 0);
+		setup_signal_action(SIGQUIT, signal_handler_heredoc, 0);
+	}
 }
 
